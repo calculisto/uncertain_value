0bc986de66fbe7ecf0da769a657fc22f9586202a
Emmanuel LE TRONG <emmanuel.le-trong@cnrs-orleans.fr>
2021-01-18 11:01:36 +0100

    Add fmt::formatting

---
+++ include/isto/uncertain_value/uncertain_value_fmt.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <fmt/format.h>
+#include "uncertain_value.hpp"
+    using isto::uncertain_value::uncertain_value_t;
+
+    template <class T>
+    struct
+fmt::formatter <uncertain_value_t <T>>
+    : fmt::formatter <T>
+{
+        using
+    base_formatter = fmt::formatter <T>;
+
+        template <class FormatContext>
+        auto
+    format (uncertain_value_t <T> const& u, FormatContext& ctx) 
+    {
+            using std::sqrt;
+        base_formatter::format (u.value, ctx);
+        format_to (ctx.out (), "±");
+        base_formatter::format (sqrt (u.uncertainty), ctx);
+        return ctx.out ();
+    }
+};
---
+++ tests/test_uncertain_value_fmt.cpp
@@ -0,0 +1,15 @@
+#include <doctest/doctest.h>
+#include "../include/isto/uncertain_value/uncertain_value_fmt.hpp"
+    using isto::uncertain_value::uncertain_value_t;
+
+TEST_CASE("uncertain_value_fmt.hpp")
+{
+        auto
+    x = uncertain_value_t { 10.1, 1.21 };
+        auto
+    s1 = fmt::format ("{}", x);
+    CHECK(s1 == "10.1±1.1");
+        auto
+    s2 = fmt::format ("{:.0}", x);
+    CHECK(s2 == "10±1");
+}
